e68e5d40c2e2036da21a07a43a5b81419639f7e0
CFussell <clayton.fussell@gmail.com>
2015-05-10 19:56:09 +0100

    update functions. Make KO more efficient. Add styling.

--- app.viewModel.js
+++ app.viewModel.js
@@ -16,31 +16,44 @@ function readCookie(name) {
 };
 
 
-//function setData(cookieData) {
-  //var houseData = JSON.parse(cookieData);
 
 
 
 
 $(document).on("ready", function(){
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
   function houseModel() {
     self = this;
 
     var houseData;
-    var cookieData = readCookie('purchaseData')
+    var cookieData = readCookie('purchaseData');
     //console.log(cookieData);
+    
     if (cookieData) {
       houseData = JSON.parse(cookieData);
+      var houseObject = {};
+      houseData.forEach(function(obj) {
+         for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                houseObject[key] = obj[key];
+            }
+         }
+      });
 
-      this.marketValue = ko.observable(houseData[0].marketValue);
-      this.share = ko.observable(houseData[1].share);
-      this.currentSavings = ko.observable(houseData[2].currentSavings);
-      this.interestRate = ko.observable(houseData[3].interestRate);
-      this.repaymentTerm = ko.observable(houseData[4].repaymentTerm);
-      this.income = ko.observable(houseData[5].income);
-      this.netMonthly = ko.observable(houseData[6].netMonthly);
-      this.serviceCharge = ko.observable(houseData[7].serviceCharge);
-      this.rentPercentage = ko.observable(houseData[8].rentPercentage);    
+      this.marketValue = ko.observable(parseInt(houseObject.marketValue));
+      this.share = ko.observable(parseInt(houseObject.share));
+      this.currentSavings = ko.observable(parseInt(houseObject.currentSavings));
+      this.movingCosts = ko.observable(parseInt(houseObject.movingCosts));
+      this.interestRate = ko.observable(parseFloat(houseObject.interestRate));
+      this.repaymentTerm = ko.observable(parseInt(houseObject.repaymentTerm));
+      this.income = ko.observable(parseInt(houseObject.income));
+      this.netMonthly = ko.observable(parseInt(houseObject.netMonthly));
+      this.serviceCharge = ko.observable(parseInt(houseObject.serviceCharge));
+      this.rentPercentage = ko.observable(parseFloat(houseObject.rentPercentage));    
 
       $("input").each(function(e){
 
@@ -57,8 +70,9 @@ $(document).on("ready", function(){
     else {
 
     this.marketValue = ko.observable();
-    this.share = ko.observable();
+    this.share = ko.observable(0);
     this.currentSavings = ko.observable();
+    this.movingCosts = ko.observable();
     this.interestRate = ko.observable();
     this.repaymentTerm = ko.observable();
     this.income = ko.observable();
@@ -68,33 +82,22 @@ $(document).on("ready", function(){
 
     };
 
-    this.movingCosts = 8000;
+    
 
-    this.deposit = ko.computed(function(){
-      return  self.currentSavings() - self.movingCosts;
-    });
-    //console.log(this.deposit())
+    
     this.shareValue = ko.computed(function(){
       return (self.marketValue() / 100) * self.share()
     });
     //console.log(this.shareValue());
 
-    this.amountBorrowing = ko.computed(function(){
-      return self.shareValue() - self.deposit();
-    });
+    
 
     this.monthlyRent = ko.computed(function(){
-      return ((self.marketValue() - self.shareValue()) * (self.rentPercentage()/100)) /12;
+      return parseFloat((((self.marketValue() - self.shareValue()) * (self.rentPercentage()/100)) /12).toFixed(2));
     });
     //console.log(self.rentPercentage()/100);
 
-    this.monthlyMortgage = ko.computed(function(){
-      return 800;
-    });
-
-    this.totalMonthly = ko.computed(function(){
-      return 800 + self.monthlyRent() + parseInt(self.serviceCharge());
-    });
+    
 
     this.stampDuty = ko.computed(function(){
       var propVal = parseInt(self.shareValue());
@@ -110,7 +113,6 @@ $(document).on("ready", function(){
         threshold: 251000,
         tax: 0.05
       };
-      //console.log((propVal - bracket2.threshold) * bracket3.tax + (bracket1.threshold * bracket2.tax));
 
       if(propVal <= bracket1.threshold) {
         return bracket1.tax
@@ -121,13 +123,90 @@ $(document).on("ready", function(){
       if (propVal > bracket2.threshold) {
         return (propVal - bracket2.threshold) * bracket3.tax + (bracket1.threshold * bracket2.tax);
       };
-
-
-      
-      //return propVal;
-
     });
-    console.log(this.stampDuty());
+
+    this.deposit = ko.computed(function(){
+      if (self.currentSavings() > 0) {
+        return self.currentSavings() - self.movingCosts() - self.stampDuty();
+      }
+      else {
+        return 0;
+      }
+    });
+
+    console.log(this.deposit());
+
+    this.amountBorrowing = ko.computed(function(){
+      return self.shareValue() - self.deposit();
+    });
+    this.depositBracket = ko.computed(function(){
+      return parseInt(self.deposit() / self.amountBorrowing() * 100);
+    });
+    this.depositEnough = ko.computed(function(){
+      return self.depositBracket() < 10 ? "alert-danger" : "alert-success";
+    });
+
+    this.monthlyMortgage = ko.computed(function(){
+    
+      var amount = parseInt(self.amountBorrowing());
+      var years = parseInt(self.repaymentTerm());
+      var rate = parseFloat(self.interestRate());
+
+      function calcPayment (amount,rate,time,balloon) {
+        if (rate<=0) {
+          if (time<=0) {
+            return amount;
+          } else {
+            return amount/time;
+          }
+        }
+        if (balloon==null) balloon = 0;
+   
+        var ln = Math.pow(1+rate,time);
+        return (amount-balloon/ln)/((1-(1/ln))/rate);
+      }
+
+      var lnAmt = Math.min(parseFloat(amount),10000000);
+      amount = lnAmt;
+      var lnRate = Math.min(parseFloat(rate),999.99);
+      rate = lnRate;
+      lnRate = lnRate/1200;
+
+      var lnYears = Math.min(parseFloat(years),100);
+      years = lnYears;
+
+      var lnPayment = Math.round(calcPayment(lnAmt,lnRate,lnYears*12));
+      return Math.floor(lnPayment,2,1);
+    });
+
+    this.totalMonthly = ko.computed(function(){
+      return self.monthlyRent() + parseInt(self.serviceCharge()) + self.monthlyMortgage();
+    });
+
+    this.savingsRequired = ko.computed(function(){
+      return self.stampDuty() + parseInt(self.movingCosts()) + self.shareValue() * 0.1;
+    });
+    this.savingsEnough = ko.computed(function(){
+      return self.savingsRequired() > self.currentSavings() ? "alert-danger" : "alert-success";
+    });
+    
+
+    this.multipleOfSalary = ko.computed(function(){
+      return parseFloat((self.amountBorrowing() / self.income()).toFixed(1));
+    });
+    this.salaryEnough = ko.computed(function(){
+      return self.multipleOfSalary() > 4 ? "alert-danger" : "alert-success";
+    });
+    this.multipleOfNetIncome = ko.computed(function(){
+      return parseFloat((self.totalMonthly() / self.netMonthly() * 100).toFixed(1));
+    });
+    this.netIncomeEnough = ko.computed(function(){
+      return self.multipleOfNetIncome() > 43 ? "alert-danger" : "alert-success";
+    });
+    this.amountLeftMonthly = ko.computed(function(){
+      return self.netMonthly() - self.totalMonthly();
+    });
+
   };
 
   ko.applyBindings(new houseModel());
